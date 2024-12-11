4fe7f056c55c7d421f8ec7ddc499aef9a608bdb6
danx0r <danbmil99@gmail.com>
2024-12-10 19:01:56 -0800

    separate xml file

--- claude3d.py
+++ claude3d.py
@@ -3,30 +3,9 @@ import numpy as np
 import glfw
 import time
 
-# Define the XML string for the MuJoCo model
-MODEL_XML = """
-<?xml version="1.0" ?>
-<mujoco>
-    <option gravity="0 0 -9.81"/>
-    <worldbody>
-        <light cutoff="100" ambient="0.25 0.25 0.25" diffuse=".7 .7 .7" specular=".85 .85 .85" dir="-0 0 -1.3" directional="true" exponent="1" pos="0 0 1.3" castshadow="false"/>
-        <!-- Ground plane -->
-        <geom type="plane" size="5 5 0.1" rgba=".49 .49 .49 1"/>
-
-        <!-- First object (fixed base) -->
-        <body pos="0 0 1" name="object1">
-            <joint type="free"/>
-            <geom type="box" size=".1 .3 .1" rgba="1 0 0 1"/>
-
-            <!-- Second object connected by hinge -->
-            <body pos="0.2 0 0" name="object2">
-                <joint type="hinge" axis="0 1 0"/>
-                <geom type="box" size=".1 .1 .3" rgba="0 1 0 1"/>
-            </body>
-        </body>
-    </worldbody>
-</mujoco>
-"""
+f = open("claude3d.xml")
+MODEL_XML = f.read()
+f.close()
 
 class InteractiveScene:
     def __init__(self):
